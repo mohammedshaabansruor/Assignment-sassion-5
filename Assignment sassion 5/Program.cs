0b98eab60b183ab1ea9a9088f792ca8a73a4b7a2
mohammedshaabansruor <shaabanmohammed490@gmail.com>
2025-01-22 22:54:42 +0200

    q1

--- Assignment sassion 5/Program.cs
+++ Assignment sassion 5/Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
+
+        public static void PassingByValue(int x)
+        {
+            int X = 100;
+        }
+        public static void PassingByRefence(ref int x)
+        {
+            int X = 100;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Q1  
+            int A = 10;
+            int B = 20;
+            PassingByValue(A);
+            Console.WriteLine($"Value of a after PassByValue: {A}");
+            PassingByRefence( ref B);
+            Console.WriteLine($"Value of a after PassByValue: {B}");
+            #endregion
         }
-    }
+    } 
+
+
+
 }
