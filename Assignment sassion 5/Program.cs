f44c0ed60c7e6a7340c84fecb39155924481e948
mohammedshaabansruor <shaabanmohammed490@gmail.com>
2025-01-22 23:04:21 +0200

    q3

--- Assignment sassion 5/Program.cs
+++ Assignment sassion 5/Program.cs
@@ -11,18 +11,41 @@
         {
             int X = 100;
         }
+        public static (int sum, int difference) Calculate(int a, int b, int c, int d)
+        {
+            int sum = a + b;
+            int difference = c - d;
+            return (sum, difference);
+        }
 
         static void Main(string[] args)
         {
 
-            #region Q1  
-            int A = 10;
-            int B = 20;
-            PassingByValue(A);
-            Console.WriteLine($"Value of a after PassByValue: {A}");
-            PassingByRefence( ref B);
-            Console.WriteLine($"Value of a after PassByValue: {B}");
+            #region Q1 
+            //int A = 10;
+            //int B = 20;
+            //PassingByValue(A);
+            //Console.WriteLine($"Value of a after PassByValue: {A}");
+            //PassingByRefence(ref B);
+            //Console.WriteLine($"Value of a after PassByValue: {B}");
             #endregion
+            #region q3  
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the third number: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the fourth number: ");
+            int num4 = int.Parse(Console.ReadLine());
+            var results = Calculate(num1, num2, num3, num4);
+            Console.WriteLine($"Sum of first and second number: {results.sum}");
+            Console.WriteLine($"Difference of third and fourth number: {results.difference}");
+            #endregion
+
         }
     } 
 
